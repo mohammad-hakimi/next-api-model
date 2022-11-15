55989ce701d9ad0cb3044bcf5d29d99e50d51b00
Mohammad Hakimi <hakimi.mohammad.n@gmail.com>
2022-11-15 19:49:18 +0330

    model created

--- services/api/cameras/camerasApi.ts
+++ services/api/cameras/camerasApi.ts
@@ -3,14 +3,10 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 const URL = (process.env.NEXT_PUBLIC_API_URL || "") + "/v1/user/cameras";
-const PLATFORM = process.env.NEXT_PUBLIC_PLATFORM || "";
 const X_API_KEY = process.env.NEXT_PUBLIC_X_API_KEY || "";
 const VERSION = process.env.NEXT_PUBLIC_VERSION || "";
-const X_AGENT = process.env.NEXT_PUBLIC_X_AGENT || "";
 const HEADERS = {
-    platform: PLATFORM,
     version: VERSION,
-    "x-agent": X_AGENT,
     "x-api-key": X_API_KEY,
 };
 
